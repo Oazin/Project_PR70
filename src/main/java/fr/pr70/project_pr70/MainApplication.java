0ddf833bdaaf4f24d15ba788938d637e88f1b9be
yrmox <nix3r10@gmail.com>
2023-10-28 16:01:05 +0200

    Test LoginApp

--- src/main/java/fr/pr70/project_pr70/MainApplication.java
+++ src/main/java/fr/pr70/project_pr70/MainApplication.java
@@ -1,4 +1,4 @@
-package fr.pr70.project_pr70.front;
+package fr.pr70.project_pr70;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("MainApp");
         stage.setScene(scene);
         stage.show();
     }
