416cfd23c95c025785f932450130c23b327423cb
yrmox <nix3r10@gmail.com>
2023-10-28 17:20:04 +0200

    Update CategoryManager

--- src/main/java/fr/pr70/project_pr70/back/CategoryManager.java
+++ src/main/java/fr/pr70/project_pr70/back/CategoryManager.java
@@ -1,21 +1,33 @@
 package fr.pr70.project_pr70.back;
 
+import javafx.scene.paint.Color;
+
 import java.util.ArrayList;
 
-public final class CategoryManager {
+public final class CategoryManager
+{
     private static ArrayList<Category> categories;
 
-    public CategoryManager(){
-        categories = new ArrayList<>();
+    public static void Init()
+    {
+        if(categories == null)
+        {
+            categories = new ArrayList<>();
+        }
     }
 
-    public static ArrayList<Category> getCategories() {
-        return categories;
+    public static void addCategories(Category _category) { CategoryManager.categories.add(_category); }
+
+    public static ArrayList<Category> getCategories() { return categories; }
+
+    public static void setCategories(ArrayList<Category> categories) { CategoryManager.categories = categories; }
+
+    public static void main(String[] argv)
+    {
+        CategoryManager.Init();
+        Category michel = new Category("Michel", Color.BLACK);
+        CategoryManager.addCategories(michel);
+        System.out.println(CategoryManager.getCategories().get(0).name);
+
     }
-
-    public static void setCategories(ArrayList<Category> categories) {
-        CategoryManager.categories = categories;
-    }
-
-
 }
