8e74766c8adaac771418ac74b45ba6b48e940403
YRMOX <74454927+YRMOX@users.noreply.github.com>
2023-11-06 18:40:41 +0100

    Update dashboard

--- src/main/java/fr/pr70/project_pr70/front/DashboardController.java
+++ src/main/java/fr/pr70/project_pr70/front/DashboardController.java
@@ -1,6 +1,17 @@
 package fr.pr70.project_pr70.front;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
+
 public class DashboardController
 {
+    @FXML
+    private VBox mainVBox;
+
+    public DashboardController()
+    {
+        String cssLayout = "-fx-border-color: red;\n-fx-border-insets: 5;\n-fx-border-width: 3;\n-fx-border-style: dashed;\n";
+        this.mainVBox.setStyle(cssLayout);
+    }
 
 }
